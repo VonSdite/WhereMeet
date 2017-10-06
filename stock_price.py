2155188959d3ec33f316f55c03bbe77cf5b759f3
Little Von <122691411@qq.com>
2017-10-06 09:35:47 +0800

    对异常进行捕获并处理

--- stock_price.py
+++ stock_price.py
@@ -6,8 +6,14 @@
 
 import requests
 import re
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import QtGui
+import time
+import sys
 
-def getOpenPrice():
+def getOpenPrice(self):
     # 股价的url
     url = "https://gupiao.baidu.com/stock/hk00700.html?from=aladingpc"
     # 请求头
@@ -27,7 +33,21 @@ def getOpenPrice():
     }
 
     # 爬取页面内容
-    res = requests.get(url=url, headers=headers)
+    try:
+        res = requests.get(url=url, headers=headers, timeout=5)
+    except:
+        # 网络异常的捕获和处理
+        openPrice = '0'
+        date = time.strftime('%Y-%m-%d')
+        message = QMessageBox(self)
+        message.setText('网络异常, 默认设置开盘价为0元')
+        message.setWindowTitle('Message')
+        message.show()
+        qr = message.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        message.move(qr.topLeft())
+        return '-'.join((date, openPrice))
     res.encoding = 'utf-8'      # 编码为utf-8
     text = res.text
 
@@ -38,9 +58,29 @@ def getOpenPrice():
     # 正则表达式获取开盘价
     pattern_price = re.compile(r'<dl><dt>今开</dt><dd class="s-up">(.*?)</dd></dl>')
     pattern_date =  re.compile(r'\d{4}-\d{2}-\d{2}')
-    openPrice = pattern_price.findall(text)[0]    # 开盘价
+    try:
+        openPrice = pattern_price.findall(text)[0]    # 开盘价
+    except IndexError:
+        # 如果还没开盘， 就提醒用户
+        openPrice = '0'
+        message = QMessageBox(self)
+        message.setText('股票还未开盘，默认设置开盘价为0元')
+        message.setWindowTitle('Message')
+        message.show()
+        qr = message.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        message.move(qr.topLeft())
+
     date = pattern_date.findall(text)[0]          # 日期
     return '-'.join((date, openPrice))
 
 if __name__ == '__main__':
-    print(getOpenPrice())
+    app = QApplication(sys.argv)
+    w = QWidget()
+    print(getOpenPrice(w))
+    w.resize(800, 500)
+    w.move(300, 300)
+    w.show()
+
+    sys.exit(app.exec_())
